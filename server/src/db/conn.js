31fb677396d81171a3be529c482a56f1aa50f11d
XIII <minibrusp@gmail.com>
2023-10-17 06:23:03 +0800

    - changed db from local to cloud

--- server/src/db/conn.js
+++ server/src/db/conn.js
@@ -1,11 +1,20 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import { MongoClient } from 'mongodb'
+import { MongoClient, ServerApiVersion  } from 'mongodb'
 
-const connectionString = process.env.ATLAS_URI || "";
+const ATLAS_URL = `mongodb+srv://${process.env.ATLAST_DB_USER}:${process.env.ATLAST_DB_PASS}@restcountriesapi.lwsrakx.mongodb.net/?retryWrites=true&w=majority`
 
-const client = new MongoClient(connectionString)
+// const connectionString = process.env.ATLAS_URI || "";
+const connectionString = ATLAS_URL || "";
+
+const client = new MongoClient(connectionString, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
 
 let conn
 
@@ -13,12 +22,12 @@ try {
   conn = await client.connect();
   console.log("Database Connection Established \n")
 } catch (error) {
-  console.error(e);
+  console.error(error);
 
   console.log(`\n\nYou must set the ATLAS_URI environment variable in the .env file`);
 
 }
 
-let db = conn.db(`${process.env.DB_NAME}`);
+let db = conn.db(`${process.env.DB_LOCAL_NAME}`);
 
 export default db;
